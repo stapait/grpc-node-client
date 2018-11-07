79eb09aa00a06dbbe40aa822f016f20eea995424
Fabio Stapait Pedarnig <fpedarnig@uolinc.com>
2018-11-07 14:21:31 -0200

    Deleting request module

--- client.js
+++ client.js
@@ -3,8 +3,6 @@ const PROTO_PATH = './calculator.proto';
 var grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
-const request = require('request');
-
 const packageDefinition = protoLoader.loadSync(
   PROTO_PATH, {
     keepCase: true,
@@ -12,15 +10,19 @@ const packageDefinition = protoLoader.loadSync(
     enums: String,
     defaults: true,
     oneofs: true
-  });
+  }
+);
 
 const calculatorPackage = grpc.loadPackageDefinition(packageDefinition).calculator;
 const calculatorService = new calculatorPackage.Calculator('localhost:50051', grpc.credentials.createInsecure());
 
 const totalRequests = process.argv[2];
 
+const MAX_PARALLEL_REQUESTS = 1000;
+
 const benchmarkGRPC = async () => {
-  let counter = 0;
+  let counter = 0;  
+  
   return new Promise((resolve) => {
     for (let i = 1; i < totalRequests; i++) {
       calculatorService.Sum({ number1: Math.round(Math.random() * 1000), number2: Math.round(Math.random() * 1000) }, (err, response) => {
@@ -35,46 +37,14 @@ const benchmarkGRPC = async () => {
   });
 };
 
-const benchmarkREST = async () => {
-  let counter = 0;
-  const url = 'http://localhost:8080/sum';
-
-  return new Promise((resolve) => {
-    for (let i = 1; i < totalRequests; i++) {
-      let args = {
-        query: {
-          number1: Math.round(Math.random() * 1000),
-          number2: Math.round(Math.random() * 1000)
-        }
-      };
-
-      request.get(url, args, (err, response) => {
-        if (err) {
-          console.log(err);
-        }
-        if (++counter === totalRequests-1) {
-          return resolve();
-        }
-      });
-    }
-  });
-};
-
-const startBenchmarks = async () => {
+const startBenchmark = async () => {
   console.log(`Starting benchmarking with ${totalRequests} requests`);
   console.log();
 
-  let startTime = new Date();
-  await benchmarkGRPC();
-  let elapsed = new Date() - startTime;
-  console.log(`RPC Total time: ${elapsed} ms - ${Math.round(totalRequests / (elapsed/1000))} requests/s`);
-
-  console.log();
-
   startTime = new Date();
-  await benchmarkREST();
+  await benchmarkGRPC();
   elapsed = new Date() - startTime;
-  console.log(`REST Total time: ${elapsed} ms - ${Math.round(totalRequests / (elapsed/1000))} requests/s`);
+  console.log(`RPC Total time: ${elapsed} ms - ${Math.round(totalRequests / (elapsed/1000))} requests/s`);
 }
 
-startBenchmarks();
+startBenchmark();
